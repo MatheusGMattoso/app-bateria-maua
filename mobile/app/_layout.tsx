ea9a41d27edc662ebafed44c148e5a7db9e241e5
MatheusGMattoso <mgarcia3818@gmail.com>
2026-04-08 15:12:22 -0300

    feat: adiciona layouts de rotas e finaliza tela de login

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,9 +1,11 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Início" }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="(auth)" />
+      <Stack.Screen name="(painel)" />
     </Stack>
   );
 }
