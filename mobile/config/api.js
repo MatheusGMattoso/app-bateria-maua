5dc88e6a867a08fdba325ef536971ae61d43942d
MatheusGMattoso <mgarcia3818@gmail.com>
2026-06-19 17:36:53 -0300

    fix: usa __DEV__ para definir BASE_URL em build de producao

--- mobile/config/api.js
+++ mobile/config/api.js
@@ -1,19 +1,6 @@
 import Constants from 'expo-constants';
-import { Platform } from 'react-native';
 
 const URL_PRODUCAO = 'https://app-bateria-maua.onrender.com/api';
-
-let usarNuvem = false;
-
-if (Platform.OS === 'web') {
-  if (typeof window === 'undefined') {
-    usarNuvem = true;
-  } else if (window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1') {
-    usarNuvem = true;
-  }
-} else {
-  usarNuvem = !__DEV__;
-}
 
 const expoHost = Constants.expoConfig?.hostUri?.split(':')[0];
 const envHost = process.env.EXPO_PUBLIC_API_HOST;
@@ -25,4 +12,4 @@ const IP_LOCAL =
 
 const URL_LOCAL = `http://${IP_LOCAL}:3000/api`;
 
-export const BASE_URL = usarNuvem ? URL_PRODUCAO : URL_LOCAL;
+export const BASE_URL = __DEV__ ? URL_LOCAL : URL_PRODUCAO;
